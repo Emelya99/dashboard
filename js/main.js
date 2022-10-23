5e30ae2582393038dc575755fcbc3f2ec8fcb11b
artur.emelyanov99 <artur.emelyanov99@gmail.com>
2022-10-23 18:47:30 +0300

    Added customer tab content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(function () {
+  // Sidebar list
+  $('.sidebar__list li').on('click', function () {
+    $('.sidebar__list li').removeClass('active');
+    $(this).addClass('active');
+  });
 
-    
-
+  // Customers pagination
+  $('.tabs__pagination li').on('click', function () {
+    $('.tabs__pagination li').removeClass('active');
+    $(this).addClass('active');
+  });
 });
